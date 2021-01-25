640950c74e5e74874e1e4775d5b1a175580f4ffe
manowakowski <nowakowski.maciej.nm@gmail.com>
2021-01-25 23:19:32 +0100

    first component and style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import ListWrapper from './components/ListWrapper/ListWrapper';
+import './index.css';
 
 const App = () => (
-    <div className="wrapper">
-        <h1 className="mainHeader">Hello World</h1>
-        <h2 className="secondaryHeader">Hello eduweb</h2>
+    <div>
+        <ListWrapper />
     </div>
 );
 
 export default App;
-
