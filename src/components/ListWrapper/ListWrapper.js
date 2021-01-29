ab385046ea8b43a94e218b29bd4be3e031f0d273
manowakowski <nowakowski.maciej.nm@gmail.com>
2021-01-29 20:13:20 +0100

    first component class

--- src/components/ListWrapper/ListWrapper.js
+++ src/components/ListWrapper/ListWrapper.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ListItem from './ListItem/ListItem';
-import './ListWrapper.css';
-import { twitterAccounts } from '../../data/twitterAccounts';
 
 const ListWrapper = () => (
-    <ul className="listWrapper__wrapper">
-        {twitterAccounts.map(item => (
-            <ListItem key={item.name} {...item} />
-        ))}
+    <ul>
+        <ListItem />
     </ul>
 );
 
